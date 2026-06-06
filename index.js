01bec87c19f60f61f5ac785b646839e9ddbd1c00
LoveDuru <elynduru@gmail.com>
2026-06-06 06:39:06 +0100

    health check feature added

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use(
 app.use(express.json());
 app.use("/api/images", express.static("upload/images"));
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use("/api/list", listRouter);
 app.use("/api/user", userRouter);
 
